965f8351e8bef8d4161ffce668987e1bf5b82745
yyhhenry <yyhhenry@foxmail.com>
2023-11-05 21:11:47 +0800

    fix: include windows.h only on windows

--- src/bin/utf8_support.cpp
+++ src/bin/utf8_support.cpp
@@ -1,9 +1,15 @@
+
+#if defined(_WIN32) || defined(_WIN64)
 #include <Windows.h>
+#endif
+
 #include <filesystem>
 #include <iostream>
 
 int main() {
+#if defined(_WIN32) || defined(_WIN64)
     SetConsoleOutputCP(CP_UTF8);
+#endif
     // This command is required to print UTF-8 characters to the console.
     // But in many other languages, utf8 is the default encoding.
 
