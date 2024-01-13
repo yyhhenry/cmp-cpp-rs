c5f4322f1bb2bf4b59b43556e6f234b007741764
yyhhenry <yyhhenry@foxmail.com>
2024-01-13 17:56:51 +0800

    Add dependencies and support for UTF-8 filenames

--- src/bin/utf8_support.cpp
+++ src/bin/utf8_support.cpp
@@ -13,7 +13,7 @@ int main() {
     // This command is required to print UTF-8 characters to the console.
     // But in many other languages, utf8 is the default encoding.
 
-    auto current_dir = std::filesystem::current_path();
+    auto current_dir = std::filesystem::current_path() / "data";
     for (auto &entry : std::filesystem::directory_iterator(current_dir)) {
         auto path = entry.path().filename().string();
         std::cout << "Filename: " << path << std::endl;
--- src/bin/utf8_support.rs
+++ src/bin/utf8_support.rs
@@ -1,12 +1,14 @@
-fn main() {
-    let current_dir = std::env::current_dir().unwrap();
-    let current_dir = current_dir.to_str().unwrap();
+use anyhow::{anyhow, Result};
+fn main() -> Result<()> {
+    let current_dir = std::env::current_dir()?.join("data");
     // read dir
-    let read_dir = std::fs::read_dir(current_dir).unwrap();
+    let read_dir = std::fs::read_dir(current_dir)?;
     for entry in read_dir {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let file_name = path.file_name().unwrap().to_str().unwrap();
+        let file_name = entry?.file_name();
+        let file_name = file_name
+            .to_str()
+            .ok_or(anyhow!("Invalid UTF-8 sequence"))?;
         println!("Filename: {}", file_name);
     }
+    Ok(())
 }
