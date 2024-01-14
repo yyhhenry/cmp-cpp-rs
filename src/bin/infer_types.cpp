c86c517b789982e49f07118bf98f433aa2e582a9
yyhhenry <yyhhenry@foxmail.com>
2024-01-14 14:13:39 +0800

    Update type inference in infer_types.cpp and infer_types.rs

--- src/bin/infer_types.cpp
+++ src/bin/infer_types.cpp
@@ -2,14 +2,14 @@
 
 void case1() {
     std::cout << "Case 1:" << std::endl;
-    auto a = 1;
+    auto a = 1; // int
     long long b = 2;
     std::cout << "Is `inferred` the same type as `typed`? " << (typeid(a) == typeid(b)) << std::endl;
-    auto _ = a + b;
+    auto _ = a + b; // long long
 }
 void case2() {
     std::cout << "Case 2:" << std::endl;
-    auto a = 1;
+    auto a = 1; // int
     long long b = 2;
     std::cout << "Is `inferred` the same type as `typed`? " << (typeid(a) == typeid(b)) << std::endl;
 }
--- src/bin/infer_types.rs
+++ src/bin/infer_types.rs
@@ -2,17 +2,17 @@ use std::any::Any;
 
 fn case1() {
     println!("Case 1:");
-    let inferred = 0;
+    let inferred = 0; // i64
     let typed: i64 = 0;
     println!(
         "Is `inferred` the same type as `typed`? {}",
         inferred.type_id() == typed.type_id()
     );
-    let _ = inferred + typed;
+    let _ = inferred + typed; // i64
 }
 fn case2() {
     println!("Case 2:");
-    let inferred = 0;
+    let inferred = 0; // i32
     let typed: i64 = 0;
     println!(
         "Is `inferred` the same type as `typed`? {}",
