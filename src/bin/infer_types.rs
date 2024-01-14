c438e051026b914c015c59495d481a5694736bac
yyhhenry <yyhhenry@foxmail.com>
2024-01-14 14:08:23 +0800

    Add infer_types.rs file with two test cases

---
+++ src/bin/infer_types.rs
@@ -0,0 +1,26 @@
+use std::any::Any;
+
+fn case1() {
+    println!("case1");
+    let inferred = 0;
+    let typed: i64 = 0;
+    println!(
+        "Is `inferred` the same type as `typed`? {}",
+        inferred.type_id() == typed.type_id()
+    );
+    let _ = inferred + typed;
+}
+fn case2() {
+    println!("case2");
+    let inferred = 0;
+    let typed: i64 = 0;
+    println!(
+        "Is `inferred` the same type as `typed`? {}",
+        inferred.type_id() == typed.type_id()
+    );
+}
+
+fn main() {
+    case1();
+    case2();
+}
